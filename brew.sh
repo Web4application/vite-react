d43683a1d9f5e0e5e16bb343859ce44761476519
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-13 19:20:04 -0700

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,2 +1,9 @@
 npm install firebase
 npm install firebase react-icons
+npm create vite@latest my-app --template react-ts
+cd my-app
+npm install
+npm start
+npx create-react-app my-app --template typescript
+cd my-app
+npm start
