0f57d980c69edf8756e978f5c3c0a87f2b85dd46
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-13 18:43:04 -0700

    Create AuthContext.tsx

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase/firebaseConfig";
+import { onAuthStateChanged, User } from "firebase/auth";
+
+interface AuthContextType {
+  user: User | null;
+}
+
+const AuthContext = createContext<AuthContextType>({ user: null });
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
