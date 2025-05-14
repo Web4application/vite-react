33f2b7992221aaef1dc71d38b807a9903dfbd224
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-13 19:30:32 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,14 @@ export default {
   },
   plugins: [],
 };
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
