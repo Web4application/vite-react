a549281f833a8e84faffcbf8b469554ded0c9019
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-13 19:15:12 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 import SignIn from "./components/SignIn";
 import ImageUpload from "./components/ImageUpload";
 import Feed from "./components/Feed";
@@ -10,13 +11,16 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <Navbar />
-        <div className="container mx-auto p-4">
-          <Routes>
-            <Route path="/" element={<Feed />} />
-            <Route path="/upload" element={<ImageUpload />} />
-            <Route path="/signin" element={<SignIn />} />
-          </Routes>
+        <div className="container">
+          <Sidebar />
+          <div className="main-content">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Feed />} />
+              <Route path="/upload" element={<ImageUpload />} />
+              <Route path="/signin" element={<SignIn />} />
+            </Routes>
+          </div>
         </div>
       </Router>
     </AuthProvider>
